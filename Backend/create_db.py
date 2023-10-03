42e18de8e55bcd0636fc0921ceded09e6ecc2cc0
Shagun Karmacharya <72041905+Shogun486@users.noreply.github.com>
2023-10-03 14:02:15 -0500

    virtual environment setup (local DB)

---
+++ Backend/create_db.py
@@ -0,0 +1,13 @@
+# Only run for the first time
+
+'''
+import mysql.connector
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="password",)
+my_cursor = mydb.cursor()
+
+my_cursor.execute("CREATE DATABASE our_users")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
+'''

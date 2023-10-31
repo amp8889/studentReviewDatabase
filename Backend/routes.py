7eec6b2cf3a710863f924ac6803ceb992ffecd6d
Aidan Pavlik <apavlik@hawk.iit.edu>
2023-10-30 19:16:05 -0500

    Minor bug fix attempts

--- Backend/routes.py
+++ Backend/routes.py
@@ -28,9 +28,9 @@ def index():
     return render_template('index.html', first_name=first_name)
 
 # Invalid route(s) or server error
-@main_bp.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
+# @main_bp.errorhandler(404)
+# def page_not_found(e):
+#     return render_template("404.html"), 404
 @main_bp.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
@@ -509,3 +509,6 @@ def addClass():
     return render_template("addClass.html", form=form)
 
 
+@main_bp.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')

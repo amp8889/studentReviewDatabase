d3c9f22ade87d70ef2e23b995705022a6b25da11
Alisha Khan <to.alisha.khan@gmail.com>
2023-11-06 19:10:20 -0600

    404 page working

--- Backend/hello.py
+++ Backend/hello.py
@@ -44,5 +44,13 @@ login_manager.login_view = 'main.f_login'
 def load_user(user_id):
     return Students.query.get(int(user_id)) or Professors.query.get(int(user_id))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
+
+x
